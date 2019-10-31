3a5f3ccebc1758d029e4519a62387c5be31630b1
Dennis Basyrov <dennis.basyrov@gmail.com>
2019-10-30 20:10:22 -0400

    Created an app, first component

---
+++ src/Container/ImageOps.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+
+export default class ImageOpsContainer extends React.Component {
+
+}
