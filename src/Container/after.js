913957379fcfa351f6e465430c8c20a7e0b3d376
Dennis Basyrov <dennis.basyrov@gmail.com>
2019-11-07 00:14:08 -0500

    logic inside two new components

---
+++ src/Container/after.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function After(props) {
+
+    return (
+      <div className="after">
+
+      </div>
+    );
+}
---
+++ src/Container/before.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Before(props) {
+
+    return (
+      <div className="before">
+
+      </div>
+    );
+};
